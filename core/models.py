a2e08970054d9d28c9a286ecb69cb368c074a4f5
virusdefender <qduliyang@outlook.com>
2014-04-28 12:38:38 +0800

    add models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Record(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    cookie = models.TextField(blank=True)
+    user_agent = models.CharField(max_length=50, blank=True)
+    ip = models.IPAddressField(blank=True)
+    url = models.URLField(balnk=True)
+    title = models.CharField(max_length=50, blank=True)
+
+    def __unicode__(self):
+        return "%s %s" % (self.create_time, self.url)
+
+
+class XssProject(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    records = models.ManyToManyField(Record, balnk=True, null=True)
+    create_time = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s" % self.title
